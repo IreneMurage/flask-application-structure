c9455bca1944e3d5c02baf22187e811a8ba5e5b4
IreneMurage <muthonimurage361@gmail.com>
2025-09-10 11:56:12 +0300

    enhance app.py structure

--- server/app.py
+++ server/app.py
@@ -3,3 +3,23 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
+
+# if __name__ == '__main__':
+#     from werkzeug.serving import run_simple
+#     run_simple(
+#         hostname='localhost',
+#         port=5555,
+#         application=application
+#     )
